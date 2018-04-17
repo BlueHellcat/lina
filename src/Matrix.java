6c857ca7a3b26e2551997b451d9b4c54146f1179
Julian Schmuhl <julian-schmuhl@web.de>
2018-04-17 13:19:00 +0200

    Added makeMatrix function in class Matrix

--- src/Matrix.java
+++ src/Matrix.java
@@ -9,19 +9,31 @@ class Matrix {
         this.data = new double[rows*cloumns];
     }
 
+    /**
+     * Initialize a new Matrix
+     * @param  m Matrix
+     * @return   Matrix
+     */
+    
+    public static Matrix makeMatrix(Matrix m) {
+      Matrix mat = new Matrix(3,3);
+      mat = mat.setData(1,0,1,0,1,1,1,0,1);
+      return mat;
+    }
+
     void setData(int... values) {
         int i = 0;
 
         for (int value : values)
             this.data[i++] = value;
-        
+
         if (i != this.data.length)
             throw new Exception("Mismatched amount of values passed to Matrix " + this.toString());
     }
 
     /**
      * Calculte the Determinant of a Matrix
-     * @param m Matrix 
+     * @param m Matrix
      */
     public static double det(Matrix m) {
         return 0.0;
