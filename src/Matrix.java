33ba4e1edc707233c934171a2086c02ea20dc6d9
sombrastudios <nils.martel@concepts-system.com>
2018-04-17 12:51:14 +0200

    Include Matrix Class

---
+++ src/Matrix.java
@@ -0,0 +1,29 @@
+
+class Matrix {
+    final int rows, columns;
+    double[] data;
+
+    Matrix(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.data = new double[rows*cloumns];
+    }
+
+    void setData(int... values) {
+        int i = 0;
+
+        for (value : values)
+            this.data[i++] = value;
+        
+        if (i != this.data.length)
+            throw new Exception("Mismatched amount of values passed to Matrix " + this.toString();
+    }
+
+    /**
+     * Calculte the Determinant of a Matrix
+     * @param m Matrix 
+     */
+    public static double det(Matrix m) {
+        return 0.0;
+    }
+}
